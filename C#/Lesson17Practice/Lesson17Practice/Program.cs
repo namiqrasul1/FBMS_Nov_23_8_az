32903d84ff30fe251ec7bc172d2602f0f9fff148
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-07-04 11:52:01 +0400

    Lesson 17. Practice, Config file

---
+++ C#/Lesson17Practice/Lesson17Practice/Program.cs
@@ -0,0 +1,44 @@
+﻿//var questions = new string[]
+//{
+//    "Azerbaycanin paytaxti haradir",
+//    "Sual 2",
+//    "Sual 3"
+//};
+
+//var answers = new string[][]
+//{
+//    ["Baki", "Gence", "Lenkeran"],
+//    ["Cavab 1", "Cavab 2", "Cavab 3"],
+//    ["Cavab 1", "Cavab 2", "Cavab 3"],
+//};
+
+//var correctAnswers = new string[]
+//{
+//    "Baki",
+//    "Cavab 2",
+//    "Cavab 3"
+//};
+
+//var rnd = new Random(); 
+
+//for (int i = 0; i < questions.Length; i++)
+//{
+//    Console.WriteLine(questions[i]);
+//    rnd.Shuffle(answers[i]);
+//    Console.WriteLine($"A. {answers[i][0]}");
+//    Console.WriteLine($"B. {answers[i][1]}");
+//    Console.WriteLine($"C. {answers[i][2]}");
+//    ConsoleKey ch = Console.ReadKey().Key;
+//    int index = ch - ConsoleKey.A;
+//    if (correctAnswers[i] == answers[i][index])
+//        Console.WriteLine("duzdu");
+//    else Console.WriteLine("sehvdi");
+//}
+
+
+using System.Configuration;
+
+var filePath = ConfigurationManager.AppSettings["filePath"];
+
+var data = Console.ReadLine();
+File.AppendAllText(filePath, data);
